409f65fbd276ef7863ddc9d6c007a58d8025c3cd
Tane Piper <piper.tane@gmail.com>
2017-11-05 22:18:00 +0100

    Working on moving to schemes and events

--- server/index.js
+++ server/index.js
@@ -9,49 +9,9 @@ const fs = require('fs');
 const Tail = require('tail').Tail;
 
 const createClient = require('../lib/create-client');
-const rgbToXy = require('../lib/rgb-to-xy');
-
-let nextStarValue = '';
-
-const STAR_TYPES = {
-    O: { kelvinRange: [25000, 40000], luminocity: [254, 254] },
-    B: { kelvinRange: [11000, 25000], luminocity: [225, 225] },
-    A: { kelvinRange: [7500, 11000], luminocity: [200, 200] },
-    F: { kelvinRange: [6000, 7500], luminocity: [150, 150] },
-    G: { kelvinRange: [5000, 6000], luminocity: [127, 127] },
-    K: { kelvinRange: [3500, 5000], luminocity: [100, 150] },
-    M: { kelvinRange: [2500, 3500], luminocity: [100, 200] },
-    L: { kelvinRange: [1300, 2500], luminocity: [100, 215] },
-    T: { kelvinRange: [500, 1300], luminocity: [200, 150] },
-    Y: { kelvinRange: [250, 500], luminocity: [100, 254] },
-    // Proto Stars
-    TTS: { kelvinRange: [2500, 3500], luminocity: [200, 200] },
-    AeBe: { kelvinRange: [7500, 1100], luminocity: [150, 150] },
-    // // Neutron Star
-    N: { red: 155, green: 176, blue: 255, brightness: 254, saturation: 254, cycle: true }
-    // H: { red: 1, green: 1, blue: 1, brightness: 0, saturation: 0 },
-    // SupermassiveBlackHole: { red: 255, green: 255, blue: 255, brightness: 20, saturation: 5 },
-    // M_RedSuperGiant: { red: 255, green: 204, blue: 111, brightness: 254, saturation: 254 }
-};
-const ct = require('color-temperature');
-
 const hubClient = createClient({ host: process.env.HUE_HUB_IP, username: process.env.HUE_HUB_USERNAME });
 
-const setStarColor = async (starClass, brightness = 127, saturation = 127) => {
-    const starData = STAR_TYPES[starClass];
-    if (!starData) {
-        console.log(`Unknown star type ${starClass}`);
-    }
-    const lights = await hubClient.lights.getAll();
-    const kelvin = Math.floor(Math.random() * (starData.kelvinRange[1] - starData.kelvinRange[0] + 1) + starData.kelvinRange[0]);
-    const colour = ct.colorTemperature2rgb(kelvin);
-    lights.forEach(async light => {
-        light.brightness = starData.luminocity[0] || brightness;
-        light.xy = rgbToXy(colour.red, colour.green, colour.blue);
-        light.saturation = starData.luminocity[1] || saturation;
-        await hubClient.lights.save(light);
-    });
-};
+const { STAR_TYPES, setStarColor } = require('../schemes/star-temperature');
 
 const getCurrentLogFile = path => {
     return new Promise(async (resolve, reject) => {
@@ -69,9 +29,9 @@ const getCurrentLogFile = path => {
     });
 };
 
-const createLogStream = async(logFile) => {
-    const logStream = new Tail(logFile);
-    logStream.on('line', async (chunk) => {
+const createLogStream = async logFile => {
+    const logStream = new Tail(logFile, { useWatchFile: true });
+    logStream.on('line', async chunk => {
         console.log('line');
         try {
             const result = JSON.parse(chunk);
@@ -80,7 +40,7 @@ const createLogStream = async(logFile) => {
                 nextStarValue = result.StarClass;
             }
             if (result.event === 'FSDJump') {
-                await setStarColor(nextStarValue)
+                await setStarColor(hubClient, nextStarValue);
             }
         } catch (e) {
             console.error(e);
@@ -106,7 +66,7 @@ const init = async (req, h) => {
             throw new Error(`No logfile`);
         }
         await createLogStream(logFile);
-        console.log('Log stream created')
+        console.log('Log stream created');
     } catch (e) {
         console.log(e);
         throw e;
